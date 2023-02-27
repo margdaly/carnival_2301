fc4c40563468ca90eb9ead46b4ef1583d4e0894b
margdaly <margaret.daly@gmail.com>
2023-02-27 09:19:20 -0700

    test: Add prefrences test to visitor

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -20,4 +20,26 @@ RSpec.describe Visitor do
       expect(visitor2.spending_money).to eq('$15')
     end
   end
+
+  describe '#preferences' do
+    it 'starts with no preferences' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      expect(visitor1.preferences).to eq([])
+    end
+
+    it 'can add preferences' do
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor1.add_preference(:gentle)
+
+      expect(visitor1.preferences).to eq([:gentle])
+    end
+
+    it 'can have more than one preference' do
+      visitor3 = Visitor.new('Penny', 64, '$15')
+      visitor3.add_preference(:gentle)
+      visitor3.add_preference(:water)
+
+      expect(visitor3.preferences).to eq([:gentle, :water])
+    end
+  end
 end
