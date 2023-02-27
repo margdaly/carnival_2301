dbb20117d81401840f5dc155259ceabe684571bc
margdaly <margaret.daly@gmail.com>
2023-02-27 11:08:15 -0700

    feat: Add #check_visitor to ride

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,7 +3,8 @@ class Ride
               :min_height,
               :admission_fee,
               :excitement,
-              :total_revenue
+              :total_revenue,
+              :ride_log
  
   def initialize(info)
     @name = info[:name]
@@ -11,5 +12,23 @@ class Ride
     @admission_fee = info[:admission_fee]
     @excitement = info[:excitement]
     @total_revenue = 0
+    @ride_log = {}
+  end
+
+  def board_rider(visitor)
+    @check_visitor
+    @total_revenue += @admission_fee
+    visitor.spending_money = visitor.spending_money.delete_prefix('$').to_i
+    visitor.spending_money = visitor.spending_money - @admission_fee
+    # count = 0
+    # if @ride_log.has_key?(visitor)
+    #   @ride_log.merge(@ride_log[visitor] = count += 1)
+    # else 
+    #   @ride_log[visitor] = count += 1
+    # end
+  end
+
+  def check_visitor(visitor)
+    visitor.preferences.include?(@excitement) && visitor.spending_money.delete_prefix('$').to_i >= @admission_fee && visitor.height >= @min_height
   end
 end
